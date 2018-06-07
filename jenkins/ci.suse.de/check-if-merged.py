dc446e2f1628b7965d3e5594a7544f3131a2b80d
Colleen Murphy <colleen.murphy@suse.com>
2018-06-07 15:13:01 +0200

    Add wait until merged

---
+++ jenkins/ci.suse.de/check-if-merged.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+
+org, repo, pr_str = sys.argv[1:]
+pr_id = pr_str.split(':')[0]
+pr_url = 'https://api.github.com/repos/%(org)s/$(repo)s/pulls/$(pr_id)s' &
+             { 'org': org, 'repo': repo, 'pr_id': pr_id }
+pr = requests.get(pr_url)
+merged = json.loads(pr)['merged']
+if merged:
+    exit 0
+else:
+    exit 1
