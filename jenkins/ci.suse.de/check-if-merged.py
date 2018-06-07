b09246efdd18a9adc27702286618a874948c5051
Colleen Murphy <colleen.murphy@suse.com>
2018-06-07 17:01:55 +0200

    json

--- jenkins/ci.suse.de/check-if-merged.py
+++ jenkins/ci.suse.de/check-if-merged.py
@@ -9,7 +9,7 @@ pr_id = pr_str.split(':')[0]
 pr_url = 'https://api.github.com/repos/%(org)s/$(repo)s/pulls/$(pr_id)s' % {
              'org': org, 'repo': repo, 'pr_id': pr_id }
 pr = requests.get(pr_url)
-merged = json.loads(pr)['merged']
+merged = json.loads(pr.json())['merged']
 if merged:
     sys.exit(0)
 else:
