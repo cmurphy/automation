408754f6f99b78c0dbd1a040a425d11dd89982d8
Colleen Murphy <colleen.murphy@suse.com>
2018-06-11 10:12:33 +0200

    Branch package to 'staging'

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -5,10 +5,11 @@ set -x
 alias osc='osc -A https://api.suse.de'
 
 if [ -d home\:comurphy\:Fake\:Cloud\:8/ardana-osconfig ] ; then
-    rm -r home\:comurphy\:Fake\:Cloud\:8/ardana-osconfig
+    rm -r home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
 fi
-osc checkout home:comurphy:Fake:Cloud:8 ardana-osconfig
-cd home\:comurphy\:Fake\:Cloud\:8/ardana-osconfig
+osc branch home:comurphy:Fake:Cloud:8 ardana-osconfig home:comurphy:Fake:Cloud:8:A
+osc checkout home:comurphy:Fake:Cloud:8:A ardana-osconfig
+cd home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
 osc rm ardana-osconfig*.obscpio
 osc service disabledrun
 osc add ardana-osconfig*.obscpio
