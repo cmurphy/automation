e7911d68196d7af14242dbe410964bec3734a81c
Colleen Murphy <colleen.murphy@suse.com>
2018-06-12 17:34:00 +0200

    Add stage to autosubmit package

---
+++ jenkins/ci.suse.de/submit-packages.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+osc='osc -A https://api.suse.de'
+for p in $($osc ls home:comurphy:Fake:Cloud:8:A) ; do
+    reqid=$($osc submitpac -m "autochecking from jenkins" home:comurphy:Fake:Cloud:8:A $p home:comurphy:Fake:Cloud:8)
+    reqid=${reqid#'created request id '}
+    $osc request accept -m "autoaccepted" $reqid
+done
