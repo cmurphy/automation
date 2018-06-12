7e3b06396c2388300e3b10bd30ec3530a61d2dad
Colleen Murphy <colleen.murphy@suse.com>
2018-06-12 16:26:27 +0200

    Add wait

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -14,6 +14,7 @@ osc rm ardana-osconfig*.obscpio
 osc service disabledrun
 osc add ardana-osconfig*.obscpio
 osc commit -m "autocheckin test"
+sleep 2
 while osc results | grep '\(building\|scheduled\)' ; do
     echo "Waiting for build results"
     sleep 10
