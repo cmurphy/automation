f57bbe18c3d933e78d5a199ddbc7b10974d2042e
Colleen Murphy <colleen.murphy@suse.com>
2018-06-12 17:33:46 +0200

    Fix update-package.sh

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -2,24 +2,24 @@
 
 set -x
 
-alias osc='osc -A https://api.suse.de'
+$osc='osc -A https://api.suse.de'
 
 if [ -d home\:comurphy\:Fake\:Cloud\:8/ardana-osconfig ] ; then
     rm -r home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
 fi
-osc branch home:comurphy:Fake:Cloud:8 ardana-osconfig home:comurphy:Fake:Cloud:8:A
-osc checkout home:comurphy:Fake:Cloud:8:A ardana-osconfig
+$osc branch home:comurphy:Fake:Cloud:8 ardana-osconfig home:comurphy:Fake:Cloud:8:A
+$osc checkout home:comurphy:Fake:Cloud:8:A ardana-osconfig
 cd home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
-osc rm ardana-osconfig*.obscpio
-osc service disabledrun
-osc add ardana-osconfig*.obscpio
-osc commit -m "autocheckin test"
+$osc rm ardana-osconfig*.obscpio
+$osc service disabledrun
+$osc add ardana-osconfig*.obscpio
+$osc commit -m "autocheckin test"
 sleep 2
-while osc results | grep '\(building\|scheduled\)' ; do
+while $osc results | grep '\(building\|scheduled\)' ; do
     echo "Waiting for build results"
     sleep 10
 done
-if ! osc results | grep succeeded ; then
+if ! $osc results | grep succeeded ; then
     echo "Build failed"
     exit 1
 fi
