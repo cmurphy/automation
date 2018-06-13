bf933a51469e375cdf7de5052b7a59ea5f21f680
Colleen Murphy <colleen.murphy@suse.com>
2018-06-13 10:30:32 +0200

    Improve osc stuff

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -7,14 +7,14 @@ $osc='osc -A https://api.suse.de'
 if [ -d home\:comurphy\:Fake\:Cloud\:8/ardana-osconfig ] ; then
     rm -r home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
 fi
-$osc branch home:comurphy:Fake:Cloud:8 ardana-osconfig home:comurphy:Fake:Cloud:8:A
+$osc copypac --keep-link home:comurphy:Fake:Cloud:8 ardana-osconfig home:comurphy:Fake:Cloud:8:A
 $osc checkout home:comurphy:Fake:Cloud:8:A ardana-osconfig
 cd home\:comurphy\:Fake\:Cloud\:8\:A/ardana-osconfig
 $osc rm ardana-osconfig*.obscpio
 $osc service disabledrun
 $osc add ardana-osconfig*.obscpio
 $osc commit -m "autocheckin test"
-sleep 2
+sleep 5
 while $osc results | grep '\(building\|scheduled\)' ; do
     echo "Waiting for build results"
     sleep 10
