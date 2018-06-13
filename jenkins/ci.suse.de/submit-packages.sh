b272b42f2c450f964111471b9ba8a40549cb9d0e
Colleen Murphy <colleen.murphy@suse.com>
2018-06-13 11:14:58 +0200

    Fix rdelete

--- jenkins/ci.suse.de/submit-packages.sh
+++ jenkins/ci.suse.de/submit-packages.sh
@@ -7,5 +7,5 @@ for p in $($osc ls home:comurphy:Fake:Cloud:8:A) ; do
     reqid=$($osc submitreq -m "autocheckin from jenkins" home:comurphy:Fake:Cloud:8:A $p home:comurphy:Fake:Cloud:8)
     reqid=${reqid#'created request id '}
     $osc request accept -m "autoaccepted" $reqid
-    $osc rdelete home:comurphy:Fake:Cloud:8:A $p
+    $osc rdelete -m "autodeleted by jenkins" home:comurphy:Fake:Cloud:8:A $p
 done
