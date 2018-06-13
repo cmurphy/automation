d0462967ea6d580b70b3a98674aa479a40e08d4d
Colleen Murphy <colleen.murphy@suse.com>
2018-06-13 10:52:01 +0200

    Fix typo

--- jenkins/ci.suse.de/submit-packages.sh
+++ jenkins/ci.suse.de/submit-packages.sh
@@ -4,7 +4,7 @@ set -x
 
 osc='osc -A https://api.suse.de'
 for p in $($osc ls home:comurphy:Fake:Cloud:8:A) ; do
-    reqid=$($osc submitreq -m "autochecking from jenkins" home:comurphy:Fake:Cloud:8:A $p home:comurphy:Fake:Cloud:8)
+    reqid=$($osc submitreq -m "autocheckin from jenkins" home:comurphy:Fake:Cloud:8:A $p home:comurphy:Fake:Cloud:8)
     reqid=${reqid#'created request id '}
     $osc request accept -m "autoaccepted" $reqid
 done
