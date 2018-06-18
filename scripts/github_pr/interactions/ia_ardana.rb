059b48dfc0177ac68aaba07acbf8b316035a925c
Colleen Murphy <colleen.murphy@suse.com>
2018-06-18 18:14:17 +0200

    Add github triggers for "ardana" repos

---
+++ scripts/github_pr/interactions/ia_ardana.rb
@@ -0,0 +1,14 @@
+require_relative 'ia_cloud'
+
+module GithubPR
+  class JenkinsJobTriggerArdanaTestbuildAction < JenkinsJobTriggerAction
+    def extra_parameters(pull, _build_mode = "")
+      {
+        github_repo: @metadata[:repository],
+        github_org: @metadata[:organization],
+        github_pr: "#{pull.number}:#{pull.head.sha}:#{pull.base.ref}",
+        job_name: "#{@metadata[:org_repo]} testbuild PR #{pull.number} #{pull.head.sha[0,8]}",
+      }
+    end
+  end
+end
