2727c3a2bea48942134ed517bc38848b155a3e09
Colleen Murphy <colleen.murphy@suse.com>
2018-06-20 12:54:51 +0200

    Use --watch

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -41,28 +41,21 @@ EOF
 function create_test_package() {
     osc copypac --keep-link $develproject $pkgname $testproject
     osc checkout $testproject $pkgname
-    cd $testproject/$pkgname
+    pushd $testproject/$pkgname
     osc rm $pkgname*.obscpio
     osc service disabledrun
     osc add $pkgname*.obscpio
     osc commit -m "autocheckin test"
-}
-
-function wait_for_build() {
-    sleep 8
-    while osc results | grep '\(building\|scheduled\|finished\)' ; do
-        echo "Waiting for build results"
-        sleep 10
-    done
+    osc results --watch
     if ! osc results | grep succeeded ; then
         echo "Build failed"
         exit 1
     fi
+    popd
 }
 
 create_project
 create_test_package
-wait_for_build
 
 cleanup
 exit 0
