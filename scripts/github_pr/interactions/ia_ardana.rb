bfe60375a5813c75c5cbd5a0b7f0500f66636b64
Colleen Murphy <colleen.murphy@suse.com>
2018-06-20 17:14:48 +0200

    use success parameter

--- scripts/github_pr/interactions/ia_ardana.rb
+++ scripts/github_pr/interactions/ia_ardana.rb
@@ -8,6 +8,7 @@ module GithubPR
         github_org: @metadata[:organization],
         github_pr: "#{pull.number}:#{pull.head.sha}:#{pull.base.ref}",
         job_name: "#{@metadata[:org_repo]} testbuild PR #{pull.number} #{pull.head.sha[0,8]}",
+        emit_success: "#{ENV['emit_success']}"
       }
     end
   end
