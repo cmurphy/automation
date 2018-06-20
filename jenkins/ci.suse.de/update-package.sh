6b4531e57a46ec641c95aadd9ba559d845bf814e
Colleen Murphy <colleen.murphy@suse.com>
2018-06-20 14:24:29 +0200

    try with addremove

--- jenkins/ci.suse.de/update-package.sh
+++ jenkins/ci.suse.de/update-package.sh
@@ -63,7 +63,8 @@ function create_test_package() {
     sed -i -e 's#<param name="revision">.*</param>#<param name="revision">test-merge</param>#' _service
     osc rm $pkgname*.obscpio
     osc service disabledrun
-    osc add $pkgname*.obscpio
+    #osc add $pkgname*.obscpio
+    osc addremove
     osc --debug --http-debug commit -m "autocheckin test"
     osc status
     osc --version
