e0ecbd259ef61bf1b384d38edcd18410e6392c13
Colleen Murphy <colleen.murphy@suse.com>
2018-07-02 11:58:09 +0200

    dont allow any stdout

--- jenkins/ci.suse.de/update-packages.sh
+++ jenkins/ci.suse.de/update-packages.sh
@@ -15,15 +15,15 @@ repos="ardana-ansible osconfig-ansible"
 export testproject=$(select_staging_project)
 for r in $repos ; do
     export github_repo=$r
-    ${SCRIPT_DIR}/update-package.sh
+    ${SCRIPT_DIR}/update-package.sh 1>&2
 done
 
 if [ $(osc -A https://api.suse.de ls $testproject | wc -l) -gt 0 ] ; then
     for p in $(osc -A https://api.suse.de ls $testproject) ; do
-        while osc results $testproject $p | grep '\(unknown\|building\|scheduled\|finished\)' ; do
+        while osc results $testproject $p | grep '\(unknown\|building\|scheduled\|finished\)' >/dev/null ; do
             sleep 5
         done
-        if osc results $testproject $p | grep failed ; then
+        if osc results $testproject $p | grep failed >/dev/null ; then
             echo "Package $p failed to build"
             exit 1
         fi
